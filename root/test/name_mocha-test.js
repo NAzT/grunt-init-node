77062b2c9a1721c1fe2625ce91dcfb16b0b6b44d
Nat <nat.wrw@gmail.com>
2014-02-15 16:02:41 +0700

    Add mock object & class variable example.

--- root/test/name_mocha-test.js
+++ root/test/name_mocha-test.js
@@ -1,18 +1,26 @@
 'use strict';
 
 require('expectations'); // exports a global, so use anywhere
+
 var sinon = require('sinon');
 var request = require('request');
 var {%= js_test_safe_name %} = require('../lib/{%= name %}.js');
 
 describe("awesome", function () {
     beforeEach(function () {
+        this.awesome = {%= js_test_safe_name %}.awesome();
+        sinon.spy(request, 'get');
+        sinon.spy(request, 'put');
+        this.callback = sinon.spy();
     });
 
     it("should be awesome", function() {
       expect({%= js_test_safe_name %}.awesome()).toEqual('awesome');
+      expect(this.awesome).toEqual('awesome');
     });
 
-    afterEach(function() {
+    afterEach(function () {
+        request.get.restore();
+        request.put.restore();
     });
 });
